2e0a3a5f21605257cc84ac6f91ba0f6d8519befc
John Warwick <jwarwick@gmail.com>
2013-07-06 13:54:51 -0400

    Added receive example functions

--- lib/elixir_recv_test.ex
+++ lib/elixir_recv_test.ex
@@ -4,6 +4,78 @@ defmodule ElixirRecvTest do
   [http://benjamintanweihao.github.io/blog/2013/07/04/elixir-for-the-lazy-impatient-and-busy-part-3-processes-101/][]
   """
 
+  @doc """
+  Spawn a process running `hola_no_recurse`
+  """
+  def spawn_no_recurse do
+    spawn(ElixirRecvTest, :hola_no_recurse, [])
+  end
+
+  @doc """
+  Spawn a process running `hola_recv_recurse`
+  """
+  def spawn_recv_recurse do
+    spawn(ElixirRecvTest, :hola_recv_recurse, [])
+  end
+
+  @doc """
+  Spawn a process running `hola_tail_recurse`
+  """
+  def spawn_tail_recurse do
+    spawn(ElixirRecvTest, :hola_tail_recurse, [])
+  end
+
+  @doc """
+  Spawn a process running `hola_tail_recurse_two`
+  """
+  def spawn_tail_recurse_two do
+    spawn(ElixirRecvTest, :hola_tail_recurse_two, [])
+  end
+
+  @doc """
+  Sends one message to `sender_pid` with the given `text`.
+  Waits for a reply of the form `{:ok, reply_test}`
+  """
+  def send_msg(sender_pid, text) do
+    sender_pid <- {self, text}
+    receive do
+      {:ok, msg} -> IO.puts "send_msg: received reply `#{msg}`"
+    end
+  end
+
+  @doc """
+  Sends `n` messages to `sender_pid` using `send_msg/1`.
+  Waits for a reply after sending each message.
+  """
+  def send_msg(sender_pid, text, n) do
+    1..n |> 
+         Enum.map(fn(x) -> send_msg(sender_pid, text <> " " <> to_binary(x)) end)
+    :ok
+  end
+
+  @doc """
+  Listen for a message of the form `{sender_pid, msg}` and 
+  sends a message back to `sender_pid` of the form `{:ok, "Received: 'msg'}`.
+
+  Does not loop
+  """
+  def hola_no_recurse do
+    IO.puts "Process: in 'hola'"
+    receive do
+      {sender, msg} ->
+        IO.puts "Process: recv msg: #{msg} from"
+        IO.inspect sender
+        sender <- {:ok, "Received: '#{msg}'"}
+    end
+    IO.puts "Process: after 'receive'"
+  end
+
+  @doc """
+  Listen for a message of the form `{sender_pid, msg}` and 
+  sends a message back to `sender_pid` of the form `{:ok, "Received: 'msg'}`.
+
+  Loops inside the `receive` block
+  """
   def hola_recv_recurse do
     IO.puts "Process: in 'hola'"
     receive do
@@ -14,5 +86,51 @@ defmodule ElixirRecvTest do
         hola_recv_recurse
     end
     IO.puts "Process: after 'receive'"
+  end
+
+  @doc """
+  Listen for a message of the form `{sender_pid, msg}` and 
+  sends a message back to `sender_pid` of the form `{:ok, "Received: 'msg'}`.
+
+  Loops after the `receive` block
+  """
+  def hola_tail_recurse do
+    IO.puts "Process: in 'hola'"
+    receive do
+      {sender, msg} ->
+        IO.puts "Process: recv msg: #{msg} from"
+        IO.inspect sender
+        sender <- {:ok, "Received: '#{msg}'"}
+    end
+    IO.puts "Process: after 'receive'"
+    hola_tail_recurse
+  end
+
+  @doc """
+  Listen for a message of the form `{sender_pid, msg}` and 
+  sends a message back to `sender_pid` of the form `{:ok, "Received: 'msg'}`.
+
+  Then listens for another message (one may already be in the mailbox...)
+  and prints it out.
+
+  Loops after the `receive` block
+  """
+  def hola_tail_recurse_two do
+    IO.puts "Process: in 'hola'"
+    receive do
+      {sender, msg} ->
+        IO.puts "Process: recv msg: #{msg} from"
+        IO.inspect sender
+        sender <- {:ok, "Received: '#{msg}'"}
+    end
+    IO.puts "Process: after 'receive'"
+
+    receive do
+      msg -> IO.puts "Process: recv second message:"
+             IO.inspect msg
+    end
+
+    IO.puts "Process after generic 'receive'"
+    hola_tail_recurse_two
   end
 end
