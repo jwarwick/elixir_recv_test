2b0a060dde30f5a5cadf8d657850291d3fb53175
John Warwick <jwarwick@gmail.com>
2013-07-06 13:25:16 -0400

    README text

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# ElixirRecvTest
+# Elixir Receive Test
+Sample code to test the behavior of Elixir's `receive` block. Inspired by Benjamin Tan's 
+blog post: http://benjamintanweihao.github.io/blog/2013/07/04/elixir-for-the-lazy-impatient-and-busy-part-3-processes-101/
 
-** TODO: Add description **
--- lib/elixir_recv_test.ex
+++ lib/elixir_recv_test.ex
@@ -1,2 +1,18 @@
 defmodule ElixirRecvTest do
+  @moduledoc """
+  Sample code inspired by Benjamin Tan's blog post about Elixir processes
+  [http://benjamintanweihao.github.io/blog/2013/07/04/elixir-for-the-lazy-impatient-and-busy-part-3-processes-101/][]
+  """
+
+  def hola_recv_recurse do
+    IO.puts "Process: in 'hola'"
+    receive do
+      {sender, msg} ->
+        IO.puts "Process: recv msg: #{msg} from"
+        IO.inspect sender
+        sender <- {:ok, "Received: '#{msg}'"}
+        hola_recv_recurse
+    end
+    IO.puts "Process: after 'receive'"
+  end
 end
