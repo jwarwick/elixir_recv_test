1cdb747b8998904d6b6c0f4bd3f0fd6bc2730534
John Warwick <jwarwick@gmail.com>
2013-07-06 14:09:20 -0400

    Added test cases

--- test/elixir_recv_test_test.exs
+++ test/elixir_recv_test_test.exs
@@ -3,7 +3,22 @@ Code.require_file "test_helper.exs", __DIR__
 defmodule ElixirRecvTestTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  # note: use assert_receive, not assert_received, since it has
+  # a non-zero timeout and the debugging IO.puts take too long
+
+  test "loop in receive" do
+    pid = ElixirRecvTest.spawn_recv_recurse
+    pid <- {self, "msg"}
+    assert_receive {:ok, _}
+    pid <- {self, "msg"}
+    assert_receive {:ok, _}
+  end
+
+  test "loop after receive" do
+    pid = ElixirRecvTest.spawn_tail_recurse
+    pid <- {self, "msg"}
+    assert_receive {:ok, _}
+    pid <- {self, "msg"}
+    assert_receive {:ok, _}
   end
 end
